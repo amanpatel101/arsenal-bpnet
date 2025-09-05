50621083bf319e37fa86503df41873ecf48079e7
Aman Patel <amanpatel101@gmail.com>
2025-09-05 00:02:28 +0000

    Transferring changes

---
+++ iterative_modeling/run_modeling_pipeline.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ENCID=$1
+MODEL_PATH=$2
+SIZE_LIST=$3
+TEMPLATE_JSON=$4
+LOCI_FILE=$5
+NEGATIVES_FILE=$6
+OUTPUT_DIR=$7
+
+ml python/3.9.0
+source /oak/stanford/groups/akundaje/patelas/sherlock_venv/arsenal-bpnet/bin/activate
+export ARSENAL_MODEL_DIR="/oak/stanford/groups/akundaje/arpitas/regulatory_lm/"
+
+mkdir $OUTPUT_DIR
+
+python make_json.py $ENCID $OUTPUT_DIR/base_config.json $TEMPLATE_JSON
+
+bash run_iterative_modeling.sh $MODEL_PATH $SIZE_LIST $TEMPLATE_JSON $LOCI_FILE $NEGATIVES_FILE $OUTPUT_DIR
---
+++ iterative_modeling/start_modeling_pipeline.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ENCID=$1
+MODEL_PATH=$2
+SIZE_LIST=$3
+TEMPLATE_JSON=$4
+LOCI_FILE=$5
+NEGATIVES_FILE=$6
+OUTPUT_DIR=$7
+
+export ARSENAL_MODEL_DIR="/oak/stanford/groups/akundaje/patelas/regulatory_lm/"
+
+sbatch --export=ALL --requeue \
+    -J $ENCID \
+    -p akundaje,gpu,owners -t 12:00:00 \
+    -G 1 -C "GPU_MEM:80GB|GPU_SKU:A100_PCIE|GPU_SKU:A100_SXM4|GPU_SKU:L40S|GPU_SKU:V100_PCIE|GPU_SKU:V100S_PCIE|GPU_SKU:V100_SXM2" \
+    --mem=60G \
+    -o $OUTPUT_DIR/.log.o \
+    -e $OUTPUT_DIR/log.e \
+    run_modeling_pipeline.sh $ENCID $MODEL_PATH $SIZE_LIST $TEMPLATE_JSON $LOCI_FILE $NEGATIVES_FILE $OUTPUT_DIR
+
+
